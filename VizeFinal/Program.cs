c1c330529366f2fcb1d956c3e7f0cf97ef4f40de
Serkan Turgay <turgay.serkan@outlook.com>
2021-01-29 18:10:21 +0300

    29.01.2021

---
+++ VizeFinal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VizeFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vize = 0;
+            int final = 0;
+            Console.WriteLine("Vize Notunuzu Giriniz:");
+            vize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Final Notunuzu Giriniz:");
+            final = Convert.ToInt32(Console.ReadLine());
+
+            int Ortalama = (vize * 4 / 10) + (final * 6 / 10);
+
+            Console.WriteLine(Ortalama);
+        }
+    }
+}
