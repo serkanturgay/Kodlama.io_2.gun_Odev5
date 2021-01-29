9af0e4c8e8db4022074825c1c12e770f3ad91027
Serkan Turgay <turgay.serkan@outlook.com>
2021-01-29 16:31:01 +0300

    29.01.2021

---
+++ HangiGun/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HangiGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int girilenSayi=0;
+            Console.WriteLine("Gun Giriniz");
+            girilenSayi = Convert.ToInt32(Console.ReadLine());
+            string[] gunler = new string[] { "Pazartesi", "Sali","Carsamba","Persembe","Cuma","Cumartesi","Pazar" };
+
+            Console.WriteLine(gunler[girilenSayi-1]);
+
+
+        }
+    }
+}
