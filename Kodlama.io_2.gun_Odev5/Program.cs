46c23b9c7bb3999c04c50f5e1c90ea6d2ad657cf
Serkan Turgay <turgay.serkan@outlook.com>
2021-01-29 16:11:56 +0300

    Initial commit

---
+++ Kodlama.io_2.gun_Odev5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kodlama.io_2.gun_Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
