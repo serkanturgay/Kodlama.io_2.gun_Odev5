94edc6675a1695841fb59796538f285139ea4a1c
Serkan Turgay <turgay.serkan@outlook.com>
2021-01-30 23:10:43 +0300

    30.01.2021

---
+++ UsAlma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UsAlma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayiyi giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ust giriniz");
+            int ust = Convert.ToInt32(Console.ReadLine());
+            int sonuc = 1;
+            for (int i = 1; i < ust+1; i++)
+            {
+
+                sonuc = sonuc * sayi;
+            }
+            Console.WriteLine(sonuc);
+        }
+
+    }
+}
