f106b44d7f7faf84783bc749659b40742261a9fb
Serkan Turgay <turgay.serkan@outlook.com>
2021-01-30 23:20:21 +0300

    30.01.2021

---
+++ AlısKar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlısKar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Urunun maliyetini giriniz : ");
+            int maliyet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("kar orani giriniz : ");
+            int kar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Satis Fiyati : " + (maliyet+maliyet*kar/100));
+
+
+        }
+    }
+}
