da2fc9b1f0f621e8771184eb3d8062690d6b5062
Serkan Turgay <turgay.serkan@outlook.com>
2021-01-30 12:22:27 +0300

    30.01.2021

---
+++ OtoparkOtomat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OtoparkOtomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kac Saat Otoparkta Kaldiniz:");
+            int otoparkSure = Convert.ToInt32(Console.ReadLine());
+            if (otoparkSure<=0)
+            {
+                Console.WriteLine("LUTFEN GECERLI SAYI GIRINIZ.");
+                int otoparkSure = Convert.ToInt32(Console.ReadLine());
+            }
+            else if (otoparkSure<=3)
+            {
+                Console.WriteLine("UCRET : 4TL");
+            }
+            else if(otoparkSure>3 && otoparkSure<=7)
+            {
+                Console.WriteLine("UCRET : 3TL");
+            }
+            else if(otoparkSure>7 && otoparkSure<=12)
+            {
+                Console.WriteLine("UCRET : 2TL");
+            }
+            else
+            {
+                Console.WriteLine("UCRET : 1TL");
+            }
+
+        }
+    }
+}
