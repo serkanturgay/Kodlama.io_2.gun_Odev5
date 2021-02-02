01db8ad05dfb6c67d4cd69d732f5112047754da6
Serkan Turgay <turgay.serkan@outlook.com>
2021-02-03 00:04:33 +0300

    2.02.2021

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint x = 0, y = 1, z;
+            for (uint i = 0; i < 85; i++)
+            {
+                z = x + y;
+                Console.WriteLine(z);
+                x = y;
+                y = z;
+            }
+        }
+    }
+}
