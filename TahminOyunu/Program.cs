c397df380ba506d892075bbd9fea1f0778eea252
Serkan Turgay <turgay.serkan@outlook.com>
2021-02-03 00:18:15 +0300

    2.02.2021

---
+++ TahminOyunu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace TahminOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random sayi = new Random();
+                int rastgele = sayi.Next(0, 10);
+                Console.WriteLine("Tahmininizi giriniz.");
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if (tahmin == rastgele)
+                {
+                    Console.WriteLine("Tebrikler ozlem Dogru Tahmin");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("mal ozlem, dogru sayi : " + rastgele);
+
+
+                }
+            }
+        }
+            
+    }
+}
